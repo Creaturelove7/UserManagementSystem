cf156c262ce79d64d1ef51cc1cb3bda118934710
scinttt <scinttt@gmail.com>
2024-12-15 17:26:54 -0500

    Initialization

---
+++ UMBackend/src/main/java/com/creaturelove/umbackend/mapper/UserMapper.java
@@ -0,0 +1,4 @@
+package com.creaturelove.umbackend.mapper;
+
+public interface UserMapper {
+}
---
+++ UMBackend/src/test/java/com/creaturelove/umbackend/UmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creaturelove.umbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

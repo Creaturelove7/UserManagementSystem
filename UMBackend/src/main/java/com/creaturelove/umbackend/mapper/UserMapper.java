dd201869cad5f2159cbbea625c2340d9c269da31
scinttt <scinttt@gmail.com>
2024-12-15 18:19:05 -0500

    create user template

--- UMBackend/src/main/java/com/creaturelove/umbackend/mapper/UserMapper.java
+++ UMBackend/src/main/java/com/creaturelove/umbackend/mapper/UserMapper.java
@@ -1,4 +1,18 @@
 package com.creaturelove.umbackend.mapper;
 
-public interface UserMapper {
+import com.creaturelove.umbackend.model.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author zhangrenren
+* @description 针对表【user】的数据库操作Mapper
+* @createDate 2024-12-15 17:58:55
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
 }
+
+
+
+
